f738248c69db8bd9edf59638c5fbde90f64286cf
rananurulker <rananurulker@gmail.com>
2024-07-01 07:27:16 +0300

    exercise

--- src/exercise03/methodOlusturmaYoutube.java
+++ src/exercise03/methodOlusturmaYoutube.java
@@ -8,6 +8,22 @@ public class methodOlusturmaYoutube {
         programn sonraki boumlerinde kullanilamk uzere metod olusturun
        .
          */
-    }
+        String isim= "CANAN";
+        String soyisim="DEMIR";
+        String isimSoyisim=isimDuzenle(isim, soyisim);
+        System.out.println("Ismin ilk girilen hali: "+isim+ " "+ soyisim);
+        System.out.println("Ismin duzenlenmis hali: "+ isimSoyisim);
+        }
 
+    private static String isimDuzenle(String isim, String soyisim) {
+        String isimDuzenlenmis=isim.substring(0,1).toUpperCase()+
+                isim.substring(1).toLowerCase();
+        String soyIsimDuzenlenmis=soyisim.substring(0,1).toUpperCase()+
+                soyisim.substring(1).toLowerCase();
+
+
+        return isimDuzenlenmis+ " "+soyIsimDuzenlenmis;
+    }
 }
+
+
