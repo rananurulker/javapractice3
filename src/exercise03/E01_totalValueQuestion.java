373020030fd411cfa7430900077765da21bbdf0b
rananurulker <rananurulker@gmail.com>
2024-07-12 01:00:43 +0300

    exercise

--- src/exercise03/E01_totalValueQuestion.java
+++ src/exercise03/E01_totalValueQuestion.java
@@ -12,18 +12,31 @@ public class E01_totalValueQuestion {
     7
     */
 
-        Scanner scanner=new Scanner(System.in);
+      // Scanner scanner=new Scanner(System.in);
+      // System.out.print("Lutfen bir sayi giriniz: ");
+      // int sayi= scanner.nextInt();
+
+      // int toplam=0;
+      //
+      // while(sayi!=0){
+      //     toplam+=sayi%10;
+
+      //     sayi/=10;
+      // }
+      // System.out.println("Toplam "+toplam);
+
+        Scanner scan=new Scanner(System.in);
         System.out.print("Lutfen bir sayi giriniz: ");
-        int num=scanner.nextInt();
+        int number= scan.nextInt();
 
         int toplam=0;
-        while(num != 0){
-            toplam += num%10;
-            num/=10;
+        while(number!=0){
+
+            toplam+=number%10;
+            number/=10;
+
         }
-
-        System.out.println("Basamaklarin toplami: "+ toplam);
-
+        System.out.println("Rakamlarin toplami: " +toplam);
 
     }
 }
---
+++ src/exercise03/E02_Soru.java
@@ -0,0 +1,24 @@
+package exercise03;
+
+public class E02_Soru {
+    public static void main(String[] args) {
+         /*2---
+        1 den 20 ye kadar tam sayıları alt alta yazan Java kodu yazınız.
+     Hint: For döngüsü kullanınız.
+     Beklenen Çıktı:
+    1
+    2
+    3
+    4
+    .
+    .
+    .
+    19
+    20
+    */
+        for (int i = 1; i <=20 ; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
